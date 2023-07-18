21abd92c13a2bc3afd1993fabc98a0313cea0908
Benja-san <beugnet.freelance@gmail.com>
2023-07-18 13:40:31 +0200

    refactor code for better use

--- src/main/java/com/bricabrac/babapi/config/WebMvcConfig.java
+++ src/main/java/com/bricabrac/babapi/config/WebMvcConfig.java
@@ -13,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         .allowedOrigins("http://localhost:4200")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
         .allowedHeaders("Content-Type", "Date", "Total-Count", "loginInfo", "Authorization")
-        .allowCredentials(true);
+        .allowCredentials(true)
+        .maxAge(3600);
     }
 }
--- src/main/java/com/bricabrac/babapi/controller/UserController.java
+++ src/main/java/com/bricabrac/babapi/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,7 +47,8 @@ public class UserController {
     }
 
     @PostMapping("/auth/login")
-    public String login(String email, String password) {
-        return userService.login(email, password);
+    public String login(@RequestBody User user) {
+        System.out.println(user.getEmail());
+        return userService.login(user.getEmail(), user.getPassword());
     }
 }
